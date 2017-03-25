056e7e8930e408b3f705d34f88a0bb84d21cb124
Felipe Romero <feliperomero3@outlook.com>
2017-03-24 18:28:34 -0700

    Add model, DbContext and databaseInitializer.

---
+++ ContosoUniversityWebApp/Models/Enrollment.cs
@@ -0,0 +1,21 @@
+﻿namespace ContosoUniversityWebApp.Models
+{
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+        public Grade Grade { get; set; }
+
+        public virtual Course Course { get; set; }
+        public virtual Student Student { get; set; }
+    }
+    public enum Grade
+    {
+        A,
+        B,
+        C,
+        D,
+        F
+    }
+}
